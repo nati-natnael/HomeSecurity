d49d9bc7a3680b61a9b985a5988bac2e0e6c3f42
nati-natnael <natnaelger@gmail.com>
2021-11-28 13:36:17 -0600

    disable object detection for testing

--- src/main/server.py
+++ src/main/server.py
@@ -43,10 +43,10 @@ class SourceStreamThread(threading.Thread):
         socket.setsockopt(zmq.CONFLATE, 1)
         socket.setsockopt_string(zmq.SUBSCRIBE, np.unicode(''))
 
-        model = tf.saved_model.load(self.model_dir + "/saved_model")
-        detect_fn = model.signatures['serving_default']
-
-        category_index = label_map_util.create_category_index_from_labelmap(self.label_dir, use_display_name=True)
+        # model = tf.saved_model.load(self.model_dir + "/saved_model")
+        # detect_fn = model.signatures['serving_default']
+        #
+        # category_index = label_map_util.create_category_index_from_labelmap(self.label_dir, use_display_name=True)
 
         stream_buffer = shared_stream_buffers[self.stream.id]
 
@@ -56,27 +56,27 @@ class SourceStreamThread(threading.Thread):
 
             frame = cv2.imdecode(np_frame, 1)
 
-            input_tensor = tf.convert_to_tensor(frame)
-            input_tensor = input_tensor[tf.newaxis, ...]
-
-            detections = detect_fn(input_tensor)
-            num_detections = int(detections.pop('num_detections'))
-
-            detections = {key: value[0, :num_detections].numpy() for key, value in detections.items()}
-            detections['num_detections'] = num_detections
-
-            detections['detection_classes'] = detections['detection_classes'].astype(np.int64)
-
-            viz_utils.visualize_boxes_and_labels_on_image_array(
-                frame,
-                detections['detection_boxes'],
-                detections['detection_classes'],
-                detections['detection_scores'],
-                category_index,
-                use_normalized_coordinates=True,
-                max_boxes_to_draw=200,
-                min_score_thresh=.60,
-                agnostic_mode=False)
+            # input_tensor = tf.convert_to_tensor(frame)
+            # input_tensor = input_tensor[tf.newaxis, ...]
+            #
+            # detections = detect_fn(input_tensor)
+            # num_detections = int(detections.pop('num_detections'))
+            #
+            # detections = {key: value[0, :num_detections].numpy() for key, value in detections.items()}
+            # detections['num_detections'] = num_detections
+            #
+            # detections['detection_classes'] = detections['detection_classes'].astype(np.int64)
+            #
+            # viz_utils.visualize_boxes_and_labels_on_image_array(
+            #     frame,
+            #     detections['detection_boxes'],
+            #     detections['detection_classes'],
+            #     detections['detection_scores'],
+            #     category_index,
+            #     use_normalized_coordinates=True,
+            #     max_boxes_to_draw=200,
+            #     min_score_thresh=.60,
+            #     agnostic_mode=False)
 
             return_val, buffer = cv2.imencode('.jpg', frame)
 
